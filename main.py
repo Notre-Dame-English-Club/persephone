6a7952d5968888c7490f54ee9afa18d7b3144490
roughweed <cavnedi@gmail.com>
2020-12-11 03:07:05 +0000

    corrected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from .checker import check
+from checker import check
 
 f = open("token.txt", "r")
 token = f.readline()
@@ -15,12 +15,15 @@ async def on_message(message):
     guild = client.get_guild(738466199263903792)
     if message.author != client.user:
         if message.channel.id == 738653551064121354:
-        splitter = message.split()
-        roll = splitter[-1]
-        name = " ".join(splitter[:-1])
-        if check(name, roll):
-            channel = client.get_channel(786787040774979585)
-            await channel.send(f"Name: {name}\nRoll:{roll}")
-
+            print(message.content)
+            splitter = message.content.split()
+            roll = splitter[-1]
+            name = " ".join(splitter[:-1])
+            if check(name, roll):
+                channel = client.get_channel(786787040774979585)
+                await channel.send(f"Name: {name}\nRoll:{roll}")
+                await message.delete()
+            else:
+                print("sad")
 
 client.run(token)
